d643da5d2c7195e62d8e1b399cba84d3223ec471
ReiWax <reiwax.pro@gmail.com>
2022-03-08 09:28:51 +0100

    Ajout d'un ville

---
+++ projetSorties/src/Form/CityFormType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\City;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CityFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class,[
+                'label' => 'Nom de le Ville',
+                'required' => true
+            ])
+            ->add('zipCode', TextType::class,[
+                'label' => 'Code Postal de la ville',
+                'required' => true
+            ])
+            ->add('Ajouter', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => City::class,
+        ]);
+    }
+}
--- projetSorties/src/Form/SearchFormType.php
+++ projetSorties/src/Form/SearchFormType.php
@@ -7,9 +7,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use App\Data\SearchData;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class SearchFormType extends AbstractType
 {
